f4ee2947389eb94c5d0282491e82371f46747c2a
Ilya <rabota003@yandex.ru>
2023-12-07 15:57:53 +0300

    one commit

--- src/main/java/org/example/Controller/SocksController.java
+++ src/main/java/org/example/Controller/SocksController.java
@@ -1,2 +1,28 @@
-package org.example.Controller;public class SocksController {
+package org.example.Controller;
+
+import org.example.Model.Socks;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api/socks")
+public class SocksController {
+    @GetMapping()
+    public String getCountSocks() {
+        return ("Возвращает общее количество носков на складе, " +
+                "соответствующих переданным в параметрах критериям запроса.");
+    }
+    @PostMapping("/income")
+    public String numberOfSocksIncome(@RequestBody Socks socks){
+        return "Регистрирует приход носков на склад.\n" +
+                "\n" +
+                "Параметры запроса передаются в " +
+                "теле запроса в виде JSON-объекта";
+    }
+    @PostMapping("/outcome")
+    public String numberOfSocksOutcome(@RequestBody Socks socks){
+        return "Регистрирует отпуск носков со склада. Здесь параметры и " +
+                "результаты аналогичные, но общее количество носков указанного" +
+                " цвета и состава не увеличивается, а уменьшается.";
+    }
+
 }
--- src/main/java/org/example/Model/Socks.java
+++ src/main/java/org/example/Model/Socks.java
@@ -1,2 +1,20 @@
-package org.example.Model;public class Socks {
+package org.example.Model;
+
+import jakarta.persistence.*;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@NoArgsConstructor
+@Table(name = "socks")
+public class Socks {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    private String color;
+    private byte cottonPart;
+    private int quantity;
+
 }
